6fb202e4821c78a42468a815107621830bb5ff89
Ismael Jarmouni Gómez <ijarmounigomez@gmail.com>
2020-11-30 16:00:23 +0100

    Update RockPaperScisors.cpp

--- RockPaperScisors.cpp
+++ RockPaperScisors.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 using namespace std;
 
-//Modulo piedra papel tijera 
-//PRE:{j1 y j2 tendran valores de 'p' para piedra, 'a' para papel y 't' para tijeras}
-//POST:{Muestra 2 si gana j2, 1 si gana j1 y 0 si hay empate}
+//ROCK PAPER SCISORS
 
-int piedrapapeltijera (char j1, char j2) {
+//PRE:{j1 y j2 will have the following values: 'p' for rock, 'a' for paper and't' for scisors}
+//POST:{The program shows 2 if p2 wins, 1 if p1 wins and 0 if there is a draw}
+
+int rockpaperscisors (char j1, char j2) {
     int res;
     if (j1==j2){
         res = 0;
     }
     else {
-        if (j1=='p') //piedra
-         if (j2=='t') //tijera    
+        if (j1=='p') //rock
+         if (j2=='t') //scisors    
             res = 1;
             else {
-                res = 2; //j2 papel  
+                res = 2; //j2 paper  
             }
         else {
-            if (j1=='t')//tijera 
-             if (j2=='a')//papel
+            if (j1=='t')//scisors
+             if (j2=='a')//paper
                 res = 1;
                 else {
-                    res = 2; // j2 piedra
+                    res = 2; // j2 rock
                 }
             else {
-                if (j1=='a')//papel
-                if (j2=='p')//piedra
+                if (j1=='a')//paper
+                if (j2=='p')//rock
                     res = 1;
                     else {
-                        res = 2; //tijera
+                        res = 2; //scisors
                     }
             }
             }
@@ -41,15 +42,17 @@ int piedrapapeltijera (char j1, char j2) {
 
 
 int main() {
-        //Pruebas del modulo
-    cout<<"j1 Piedra , J2 Piedra "<<piedrapapeltijera('p','p')<<endl;
-    cout<<"j1 Piedra , J2 Papel "<<piedrapapeltijera('p','a')<<endl;
-    cout<<"j1 Piedra , J2 Tijera "<<piedrapapeltijera('p','t')<<endl;
-    cout<<"j1 Papel , J2 Piedra "<<piedrapapeltijera('a','p')<<endl;
-    cout<<"j1 Papel , J2 Papel "<<piedrapapeltijera('a','a')<<endl;
-    cout<<"j1 Papel , J2 Tijera "<<piedrapapeltijera('a','t')<<endl;
-    cout<<"j1 Tijera , J2 Piedra "<<piedrapapeltijera('t','p')<<endl;
-    cout<<"j1 Tijera , J2 Papel "<<piedrapapeltijera('t','a')<<endl;
-    cout<<"j1 Tijera , J2 Tijera "<<piedrapapeltijera('t','t')<<endl;
+                
+                   //Module's testing program
+    
+    cout<<"j1 Rock , J2 Rock "<<rockpaperscisors('p','p')<<endl;
+    cout<<"j1 Rock , J2 Paper "<<rockpaperscisors('p','a')<<endl;
+    cout<<"j1 Rock , J2 Tijera "<<rockpaperscisors('p','t')<<endl;
+    cout<<"j1 Paper , J2 Rock "<<rockpaperscisors('a','p')<<endl;
+    cout<<"j1 Paper , J2 Paper "<<rockpaperscisors('a','a')<<endl;
+    cout<<"j1 Paper , J2 Scisors "<<rockpaperscisors('a','t')<<endl;
+    cout<<"j1 Scisors , J2 Rock "<<rockpaperscisors('t','p')<<endl;
+    cout<<"j1 Scisors , J2 Paper "<<rockpaperscisors('t','a')<<endl;
+    cout<<"j1 Scisors , J2 Scisors "<<rockpaperscisors('t','t')<<endl;
     return 0;
 }
